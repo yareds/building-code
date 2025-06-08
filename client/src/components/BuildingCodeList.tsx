a37a7360577c7a64aec37ccb8d9cb087d3e0d997
yareds <yared.abegaz@gmail.com>
2025-06-08 13:46:11 -0400

    Add Netlify configuration files

--- client/src/components/BuildingCodeList.tsx
+++ client/src/components/BuildingCodeList.tsx
@@ -9,7 +9,7 @@ import {
   IconButton,
   Grid,
   Box,
-  Paper
+  Paper,
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
